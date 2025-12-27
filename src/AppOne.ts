b14a6848d4b7eed5cd8df19da6c18deb71601fc4
Rolf Hammerstad <epostenmin@gmail.com>
2025-12-27 18:55:14 +0100

    initial

--- src/AppOne.ts
+++ src/AppOne.ts
@@ -1,94 +1,105 @@
-import * as BABYLON from 'babylonjs'
+// import * as BABYLON from "babylonjs";
+import {
+	MeshBuilder,
+	StandardMaterial,
+	Engine,
+	Scene,
+	ArcRotateCamera,
+	Vector3,
+	HemisphericLight,
+	SceneLoader,
+	Color3,
+	Color4,
+	Texture,
+} from 'babylonjs'
+// import {SceneLoader} from 'babylonjs-loaders'
+import 'babylonjs-loaders'
+
+// import {ImportMeshAsync} from "babylonjs/Meshes/meshImporters/importMeshAsync";
+
 export class AppOne {
-    engine: BABYLON.Engine;
-    scene: BABYLON.Scene;
+	engine: Engine
+	scene: Scene
 
-    constructor(readonly canvas: HTMLCanvasElement) {
-        this.engine = new BABYLON.Engine(canvas)
-        window.addEventListener('resize', () => {
-            this.engine.resize();
-        });
-        this.scene = createScene(this.engine, this.canvas)
+	constructor(readonly canvas: HTMLCanvasElement) {
+		this.engine = new Engine(canvas)
+		window.addEventListener('resize', () => {
+			this.engine.resize()
+		})
+		this.scene = createScene(this.engine, this.canvas)
+	}
 
-    }
+	debug(debugOn: boolean = true) {
+		if (debugOn) {
+			this.scene.debugLayer.show({ overlay: true })
+		} else {
+			this.scene.debugLayer.hide()
+		}
+	}
 
-    debug(debugOn: boolean = true) {
-        if (debugOn) {
-            this.scene.debugLayer.show({ overlay: true });
-        } else {
-            this.scene.debugLayer.hide();
-        }
-    }
-
-    run() {
-        this.debug(true);
-        this.engine.runRenderLoop(() => {
-            this.scene.render();
-        });
-    }
-
-
-
+	run() {
+		this.debug(true)
+		this.engine.runRenderLoop(() => {
+			this.scene.render()
+		})
+	}
 }
 
+function createScene(engine: Engine, canvas: HTMLCanvasElement) {
+	const scene = new Scene(engine)
+	scene.clearColor = new Color4(0, 0, 0, 1)
 
-var createScene = function (engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
-    // this is the default code from the playground:
+	// Camera
+	const camera = new ArcRotateCamera(
+		'ArcRotateCamera',
+		Math.PI / 2,
+		Math.PI / 2.5,
+		10,
+		Vector3.Zero(),
+		scene,
+	)
+	camera.setTarget(Vector3.Zero())
+	camera.attachControl(canvas, true)
 
-    // This creates a basic Babylon Scene object (non-mesh)
-    var scene = new BABYLON.Scene(engine);
+	const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene)
+	light.intensity = 0.7
 
-    // This creates and positions a free camera (non-mesh)
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+	const sphere = MeshBuilder.CreateSphere('sphere', { diameter: 1, segments: 32 }, scene)
+	let startPos = 1
+	sphere.position.y = startPos
 
-    // This targets the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
+	const ground = MeshBuilder.CreateGround('ground', { width: 6, height: 6 }, scene)
+	const groundMaterial = new StandardMaterial('groundMaterial', scene)
+	groundMaterial.diffuseColor = Color3.FromHexString('#80cc80')
+	ground.material = groundMaterial
+	groundMaterial.bumpTexture = new Texture('./normal.jpg', scene)
 
-    // This attaches the camera to the canvas
-    camera.attachControl(canvas, true);
+	SceneLoader.ImportMeshAsync('', 'models/', 'head1.glb', scene).then((result: any) => {
+		console.log('Model loaded:', result.meshes)
+		result.meshes[0].position = new Vector3(2, 0, 0)
+	})
 
-    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+	const redMaterial = new StandardMaterial('redMaterial', scene)
+	redMaterial.diffuseColor = new Color3(1, 0, 0)
+	sphere.material = redMaterial
 
-    // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.7;
+	let sphereVelocity = 0
+	const gravity = 0.0049
+	const reboundLoss = 0.1
 
-    // Our built-in 'sphere' shape.
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
-    // Move the sphere upward 1/2 its height
-    let startPos = 2;
-    sphere.position.y = startPos;
+	scene.registerBeforeRender(() => {
+		sphereVelocity += gravity
+		let newY = sphere.position.y - sphereVelocity
+		sphere.position.y -= sphereVelocity
+		if (newY < 1) {
+			sphereVelocity = (reboundLoss - 1) * sphereVelocity
+			newY = 1
+		}
+		sphere.position.y = newY
+		if (Math.abs(sphereVelocity) <= gravity && newY < 1 + gravity) {
+			sphere.position.y = startPos++
+		}
+	})
 
-    // Our built-in 'ground' shape.
-    var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
-    var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
-    groundMaterial.diffuseColor = new BABYLON.Color3(0.5, 0.8, 0.5); // RGB for a greenish color
-    ground.material = groundMaterial;
-    groundMaterial.bumpTexture = new BABYLON.Texture("./normal.jpg", scene);
-    //groundMaterial.bumpTexture.level = 0.125;    
-
-
-    var redMaterial = new BABYLON.StandardMaterial("redMaterial", scene);
-    redMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0); // RGB for red
-    sphere.material = redMaterial;
-
-    var sphereVelocity = 0;
-    var gravity = 0.009;
-    var reboundLoss = 0.1;
-
-    scene.registerBeforeRender(() => {
-        sphereVelocity += gravity;
-        let newY = sphere.position.y - sphereVelocity;
-        sphere.position.y -= sphereVelocity
-        if (newY < 1) {
-            sphereVelocity = (reboundLoss - 1) * sphereVelocity;
-            newY = 1;
-        }
-        sphere.position.y = newY;
-        if (Math.abs(sphereVelocity) <= gravity && newY < 1 + gravity) {
-            sphere.position.y = startPos++;
-        }
-    });
-
-    return scene;
-};
+	return scene
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { AppOne as App } from './AppOne';
+import { AppOne as App } from './AppOne'
 
-console.log(`main.ts starting ${App.name}`);
+console.log(`main.ts starting ${App.name}`)
 window.addEventListener('DOMContentLoaded', () => {
-    let canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
-    let app = new App(canvas);
-    app.run();
-});
+	let canvas = document.getElementById('renderCanvas') as any as HTMLCanvasElement
+	let app = new App(canvas)
+	app.run()
+})
